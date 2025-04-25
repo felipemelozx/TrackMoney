9e7fee0477537e520ed55e0a8cc738428085bd6e
Felipe Melo <felipemelozx@gmail.com>
2025-04-25 20:29:57 -0300

    :tada: First Commit

---
+++ src/main/java/fun/trackmoney/TrackMoneyApplication.java
@@ -0,0 +1,13 @@
+package fun.trackmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fun/TrackMoney/TrackMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.TrackMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
