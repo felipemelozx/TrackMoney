cdc08666ad189f11d3e4ff5342a5db1981889276
Felipe Melo <felipemelozx@gmail.com>
2025-05-06 15:24:57 -0300

    test: add unit tests for UserService registration logic

--- src/test/java/fun/trackmoney/TrackMoneyApplicationTests.java
+++ src/test/java/fun/trackmoney/TrackMoneyApplicationTests.java
@@ -1,6 +1,5 @@
-package fun.TrackMoney;
+package fun.trackmoney;
 
-import fun.trackmoney.TrackMoneyApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
