5486221f8cdb1e7d64e012040973ef1eb92ba243
Felipe Melo <felipemelozx@gmail.com>
2025-05-06 15:23:41 -0300

    feat: implement authentication module with JWT and security configuration

---
+++ src/main/java/fun/trackmoney/auth/infra/jwt/JwtService.java
@@ -0,0 +1,47 @@
+package fun.trackmoney.auth.infra.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class JwtService {
+  @Value("${api.secret.key}")
+  private String secret;
+
+  public String generateToken(String email){
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(secret);
+      return JWT.create()
+          .withIssuer("API-AUTH-TRACK-MONEY")
+          .withClaim("roles", "USER_ROLES")
+          .withSubject(email)
+          .withExpiresAt(getExpires())
+          .sign(algorithm);
+    } catch (JWTCreationException e){
+      throw new RuntimeException("Error while authenticating.");
+    }
+  }
+  public String validateToken(String token){
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(secret);
+      return JWT.require(algorithm)
+          .withIssuer("API-AUTH")
+          .build()
+          .verify(token)
+          .getSubject();
+    }catch (JWTVerificationException e){
+      return null;
+    }
+  }
+
+  private Instant getExpires() {
+    return LocalDateTime.now().plusHours(3).toInstant(ZoneOffset.ofHours(-3));
+  }
+}
