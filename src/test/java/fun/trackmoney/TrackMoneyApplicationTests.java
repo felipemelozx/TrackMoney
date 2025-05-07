ceb8352f0cc04a23a9093fe60c0b05c47a4dbb5f
Felipe Melo <felipemelozx@gmail.com>
2025-05-07 15:21:20 -0300

    feat: Migrate test databse H2 to Docker compose

--- src/test/java/fun/trackmoney/TrackMoneyApplicationTests.java
+++ src/test/java/fun/trackmoney/TrackMoneyApplicationTests.java
@@ -2,7 +2,6 @@ package fun.trackmoney;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = TrackMoneyApplication.class)
 class TrackMoneyApplicationTests {
