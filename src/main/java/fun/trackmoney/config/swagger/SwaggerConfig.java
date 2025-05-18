d78cbb98d00556b569c14bb1e98fc0c97cd3ad38
Felipe Melo <felipemelozx@gmail.com>
2025-05-18 08:48:00 -0300

    feat: add auth config in swagger

--- src/main/java/fun/trackmoney/config/swagger/SwaggerConfig.java
+++ src/main/java/fun/trackmoney/config/swagger/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package fun.trackmoney.config.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,6 +22,13 @@ public class SwaggerConfig {
                 "for financial resources (transactions, budgets, recurring accounts), and more. " +
                 "The backend is built using Java 17, Spring Boot, Spring Security, and follows " +
                 "the Clean Architecture principles. It is integrated with a PostgreSQL database " +
-                "and includes a CI/CD pipeline for continuous integration and deployment."));
+                "and includes a CI/CD pipeline for continuous integration and deployment."))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new Components()
+            .addSecuritySchemes("bearerAuth",
+                new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")));
   }
 }
