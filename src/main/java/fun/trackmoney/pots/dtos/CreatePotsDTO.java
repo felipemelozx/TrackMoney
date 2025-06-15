dfbf0aff952b73dd98b9f80f6fb5f398cbafba24
Felipe Melo <felipemelozx@gmail.com>
2025-06-15 19:20:53 -0300

    chore: fix type error

---
+++ src/main/java/fun/trackmoney/pots/dtos/CreatePotsDTO.java
@@ -0,0 +1,4 @@
+package fun.trackmoney.pots.dtos;
+
+public record CreatePotsDTO() {
+}
--- src/main/resources/db/migration/V4__create_pots_schema.sql
+++ src/main/resources/db/migration/V4__create_pots_schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE tb_pots (
-    pot_id BIGINT PRIMARY KEY,
+    pot_id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     description VARCHAR(255),
     target_amount BIGINT,
     current_amount BIGINT,
-    account_id BIGINT,
+    account_id SERIAL,
     CONSTRAINT fk_pots_account FOREIGN KEY (account_id) REFERENCES tb_account(account_id)
 );
