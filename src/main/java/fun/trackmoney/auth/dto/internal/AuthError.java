34913e9cf8589a9ee495cf469a0bcfbef3c5182b
Felipe Melo <felipemelozx@gmail.com>
2025-08-28 14:39:44 -0300

    feat: add authentication result classes for login process

---
+++ src/main/java/fun/trackmoney/auth/dto/internal/AuthError.java
@@ -0,0 +1,9 @@
+package fun.trackmoney.auth.dto.internal;
+
+public enum AuthError {
+    EMAIL_NOT_VERIFIED,
+    INVALID_CREDENTIALS,
+    REFRESH_TOKEN_INVALID,
+    USER_NOT_REGISTER,
+    USER_IS_VERIFIED
+}
