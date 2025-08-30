05a6a6ba1233e4e925f549d57242a44bd325ac66
Felipe Melo <felipemelozx@gmail.com>
2025-08-30 17:18:03 -0300

    refactor: remove embedded Redis dependency and simplify test setup

--- src/test/java/fun/trackmoney/TrackMoneyApplicationTests.java
+++ src/test/java/fun/trackmoney/TrackMoneyApplicationTests.java
@@ -1,28 +1,17 @@
 package fun.trackmoney;
 
-import fun.trackmoney.config.EmbeddedRedisConfig;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(classes = TrackMoneyApplication.class)
+@AutoConfigureMockMvc
 class TrackMoneyApplicationTests {
 
   @Autowired
   private MockMvc mockMvc;
-
-  @BeforeAll
-  static void up() {
-    EmbeddedRedisConfig.startRedis();
-  }
-
-  @AfterAll
-  static void down() {
-    EmbeddedRedisConfig.stopRedis();
-  }
 
 	@Test
 	void contextLoads() {
