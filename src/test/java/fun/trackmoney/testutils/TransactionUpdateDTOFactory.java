872292e6ee48ed937b1e3b952bd53273be9eb0eb
Felipe Melo <felipemelozx@gmail.com>
2025-11-14 15:10:14 -0300

    test: replace inline TransactionUpdateDTO with TransactionUpdateDTOFactory in tests

---
+++ src/test/java/fun/trackmoney/testutils/TransactionUpdateDTOFactory.java
@@ -0,0 +1,21 @@
+package fun.trackmoney.testutils;
+
+import fun.trackmoney.enums.TransactionType;
+import fun.trackmoney.transaction.dto.TransactionUpdateDTO;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TransactionUpdateDTOFactory {
+
+    public static TransactionUpdateDTO defaultUpdateTransaction() {
+        return new TransactionUpdateDTO(
+            "Updated Transaction Name",
+            "Updated transaction description",
+            new BigDecimal("199.99"),
+            1,
+            TransactionType.EXPENSE,
+            LocalDateTime.now()
+        );
+    }
+}
