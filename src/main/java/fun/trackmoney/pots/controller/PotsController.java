d74645357e89dcf5e9f7a8b1ccf8ffb31699d843
Felipe Melo <felipemelozx@gmail.com>
2025-11-20 19:22:38 -0300

    refactor: implement ColorPick enum and update PotsController and PotsService to use currentUser for pot creation and retrieval

--- src/main/java/fun/trackmoney/pots/controller/PotsController.java
+++ src/main/java/fun/trackmoney/pots/controller/PotsController.java
@@ -3,13 +3,17 @@ package fun.trackmoney.pots.controller;
 import fun.trackmoney.pots.dtos.CreatePotsDTO;
 import fun.trackmoney.pots.dtos.PotsResponseDTO;
 import fun.trackmoney.pots.service.PotsService;
+import fun.trackmoney.user.entity.UserEntity;
 import fun.trackmoney.utils.response.ApiResponse;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,23 +28,27 @@ public class PotsController {
     this.potsService = potsService;
   }
 
-  @GetMapping("/{id}")
-  public ResponseEntity<ApiResponse<List<PotsResponseDTO>>> getPots(@PathVariable Integer id) {
+  @PostMapping
+  public ResponseEntity<ApiResponse<PotsResponseDTO>> createPots(@Valid @RequestBody CreatePotsDTO dto,
+                                                                 @AuthenticationPrincipal UserEntity currentUser) {
+
+    var data = potsService.create(dto, currentUser);
+    return ResponseEntity.ok().body(
+        ApiResponse.<PotsResponseDTO>success()
+            .message("Pots register successfully")
+            .data(data)
+            .build());
+  }
+
+  @GetMapping
+  public ResponseEntity<ApiResponse<List<PotsResponseDTO>>> getPots(@AuthenticationPrincipal UserEntity currentUser) {
     return ResponseEntity.ok(
         ApiResponse.<List<PotsResponseDTO>>success()
             .message("Pots retrieved successfully")
-            .data(potsService.findAllPots(id))
+            .data(potsService.findAllPots(currentUser))
             .build()
     );
   }
 
-  @PostMapping
-  public ResponseEntity<ApiResponse<PotsResponseDTO>> createPots(@RequestBody CreatePotsDTO dto) {
-    return ResponseEntity.ok(
-        ApiResponse.<PotsResponseDTO>success()
-            .message("Pots register successfully")
-            .data(potsService.create(dto))
-            .build()
-    );
-  }
+
 }
--- src/main/java/fun/trackmoney/pots/service/PotsService.java
+++ src/main/java/fun/trackmoney/pots/service/PotsService.java
@@ -1,12 +1,12 @@
 package fun.trackmoney.pots.service;
 
-import fun.trackmoney.account.exception.AccountNotFoundException;
-import fun.trackmoney.account.repository.AccountRepository;
 import fun.trackmoney.pots.dtos.CreatePotsDTO;
 import fun.trackmoney.pots.dtos.PotsResponseDTO;
 import fun.trackmoney.pots.mapper.PotsMapper;
 import fun.trackmoney.pots.repository.PotsRepository;
+import fun.trackmoney.user.entity.UserEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,23 +15,22 @@ public class PotsService {
 
   private final PotsRepository potsRepository;
   private final PotsMapper potsMapper;
-  private final AccountRepository accountRepository;
 
-  public PotsService(PotsRepository potsRepository, PotsMapper potsMapper, AccountRepository accountRepository) {
+  public PotsService(PotsRepository potsRepository, PotsMapper potsMapper) {
     this.potsRepository = potsRepository;
     this.potsMapper = potsMapper;
-    this.accountRepository = accountRepository;
   }
 
-  public List<PotsResponseDTO> findAllPots(Integer accountId) {
-    return potsMapper.listToResponse(potsRepository.findAllPotsByAccountId(accountId));
-  }
-
-  public PotsResponseDTO create(CreatePotsDTO dto) {
+  @Transactional
+  public PotsResponseDTO create(CreatePotsDTO dto, UserEntity currentUser) {
     var entity = potsMapper.toEntity(dto);
-    var account = accountRepository.findById(dto.accountId())
-        .orElseThrow(() -> new AccountNotFoundException("Account not found exception for id: " + dto.accountId()));
+    var account = currentUser.getAccount();
     entity.setAccount(account);
-    return potsMapper.toResponse(potsRepository.save(entity));
+    var potsResponse = potsRepository.save(entity);
+    return potsMapper.toResponse(potsResponse);
+  }
+
+  public List<PotsResponseDTO> findAllPots(UserEntity currentUser) {
+    return potsMapper.listToResponse(potsRepository.findAllPotsByAccountId(currentUser.getAccount()));
   }
 }
